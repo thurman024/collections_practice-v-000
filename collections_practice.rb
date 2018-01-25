0c8ebc906683a1f7a506b430954abe8434c92b7f
Stephen Thurman <thurman024@gmail.com>
2018-01-25 18:03:19 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -42,3 +42,15 @@ end
 def sum_array(array)
   array.inject {|sum, n| sum + n }
 end
+
+def add_s(array)
+  array.each_with_index do |element, i|
+    plural = []
+    if i != 1
+      plural << element + "s"
+    end
+  end
+  plural
+end
+    
+    
